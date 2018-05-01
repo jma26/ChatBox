be09deac09c15c4c59a70a09d80d7078cddbaa8b
jma26 <j.masonoma17@gmail.com>
2018-04-30 23:56:28 -0700

    Add socket.io functionality- message displays on console.log

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express");
-
 const app = express(); 
 
 // Serve static files css, js, or images
 app.use(express.static("public"));
 
+// Default route to send
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8000, function() {
-    console.log("Server is listening on port 8000");
+// Server port
+const server = app.listen(8000);
+
+// Socket.io
+const io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function(socket) {
+    console.log("User connected!");
+    // Receive chat mesasge
+    socket.on("chat message", function(message) {
+        console.log("Message is: ", message);
+    })
+    // Fire disconnect
+    socket.on("disconnect", function() {
+        console.log("User disconnected!");
+    })
 })
